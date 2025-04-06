53be01b594a2319141905db2ff639f3b94240b21
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-04-06 18:31:15 -0300

    feat: added fetch to the API

--- src/components/headerComponents/searchBar.tsx
+++ src/components/headerComponents/searchBar.tsx
@@ -4,6 +4,23 @@ import { Search } from "lucide-react";
 export default function SearchBar() {
   // State to manage the search query, will be used to filter the movies with the API later
   const [query, setQuery] = useState("");
+
+  const fetchData = (value: string) => {
+    fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&language=pt-BR&api_key=12923231fddd461a9280cdc286a6bee5&query=${value}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  const handleChange = (value: string) => {
+    setQuery(value);
+    fetchData(value);
+  }
+
   return (
     <div className="relative flex items-center w-full max-w-md">
       <input
@@ -11,7 +28,7 @@ export default function SearchBar() {
         type="text"
         placeholder="Pesquisar produções..."
         value={query}
-        onChange={(e) => {setQuery(e.target.value); 
+        onChange={(e) => {handleChange(e.target.value); 
             console.log(e.target.value); 
         }}
       />
