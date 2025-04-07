e8bc0b2fd251548ca32ae14f37882f61be5e7592
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-04-06 21:59:32 -0300

    fix: changing mobile menu margin

--- src/components/headerComponents/mobileHeader.tsx
+++ src/components/headerComponents/mobileHeader.tsx
@@ -33,22 +33,22 @@ export default function MobileHeaderMenu({ Menus }: { Menus: MenuItem[] }) {
             </button>
 
             <motion.div 
-                className="fixed left-0 right-0 top-24 overflow-y-auto h-full bg-[#1E1A1A] text-white p-6 z-[50]"
-                initial={{ x: "-100%" }}
-                animate={{ x: isOpen ? "0%" : "-100%" }}
-            >
-                <ul>
-                    {Menus?.map(({ name, link }) => (
-                        <li key={name}>
-                            <a href={link}>
-                                <span className="flex items-center justify-between p-4 text-lg hover:bg-white/5 rounded-md cursor-pointer relative">
-                                    {name}
-                                </span>
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </motion.div>
+    className="fixed left-0 top-0 w-full h-full overflow-y-auto bg-[#1E1A1A] text-white p-6 z-[50]"
+    initial={{ x: "-100%" }}
+    animate={{ x: isOpen ? "0%" : "-100%" }}
+        transition={{ type: "spring", stiffness: 300, damping: 30 }}>
+    <ul className="mt-8">
+        {Menus?.map(({ name, link }) => (
+            <li key={name}>
+                <a href={link}>
+                    <span className="flex items-center justify-between p-4 text-lg hover:bg-white/5 rounded-md cursor-pointer relative">
+                        {name}
+                    </span>
+                </a>
+            </li>
+        ))}
+    </ul>
+</motion.div>
         </div>
     );
 }
