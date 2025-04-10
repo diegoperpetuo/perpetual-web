b8d9a331fd4437df84aac2e7d3d2d99645af437d
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-04-09 23:07:51 -0300

    feat: adding routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
 import HeroBanner from "./components/heroBanner";
+import Details from "./components/detailsPage";
 
 function App() {
   return (
     <div>
       <Header/>
-      <HeroBanner/>
+      <Routes>
+        <Route path='/' element={<HeroBanner/>}/>
+        <Route path='/detalhes/:id' element={<Details/>}/>
+      </Routes>
     </div>
   );
 }
